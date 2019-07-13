ae84159762fc1cb17044ef3631cec78302348ad7
Matt Werner <mrwerner392@gmail.com>
2019-07-13 18:07:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
-# My Code here....
+# negativize each value in an array
+def map_to_negativize(array)
+  neg_array = []
+  counter = 0
+
+  while counter < array.length do
+    neg_array.push(array[counter] * (-1))
+    counter += 1
+  end
+
+  neg_array
+end
+
+def map_to_no_change(array)
+
+end
+
+def map_to_double(array)
+
+end
+
+def map_to_square(array)
+
+end
+
+def reduce_to_total(array)
+
+end
+
+def reduce_to_all_true(array)
+
+end
+
+def reduce_to_any_true(array)
+
+end
